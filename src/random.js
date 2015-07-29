f5337a935b16abb462e99edaae1fb4f7e44e0ea5
Hendry Sadrak <hendry.sadrak@outlook.com>
2015-07-29 21:56:33 +0300

    EDIT reformat code

--- src/random.js
+++ src/random.js
@@ -5,8 +5,17 @@
  */
 (function randomConstruct() {
     "use strict";
+    /**
+     * /////////////////////////////////////////////////////////////////////
+     * Variables
+     */
+    var _randomArray = [];
+
+    var _currentRandomIndex = 0;
+    var _currentRandomChangeIndex = 0;
 
     /**
+     * /////////////////////////////////////////////////////////////////////
      * Options
      */
     var _defaults = {
@@ -16,13 +25,14 @@
     };
 
     var _options = window.fastrandomOptions || {};
-    for (var option in _defaults) {
-        if (_defaults.hasOwnProperty(option) && !_options.hasOwnProperty(option)) {
-            _options[option] = _defaults[option];
+    for (var key in _defaults) {
+        if (_defaults.hasOwnProperty(key) && !_options.hasOwnProperty(key)) {
+            _options[key] = _defaults[key];
         }
     }
 
     /**
+     * /////////////////////////////////////////////////////////////////////
      * Remove the global options var
      */
     if (_options.removeOptions && (typeof window.fastrandomOptions !== "undefined")) {
@@ -30,14 +40,7 @@
     }
 
     /**
-     * Vars
-     */
-    var _randomArray = [];
-
-    var _currentRandomIndex = 0;
-    var _currentRandomChangeIndex = 0;
-
-    /**
+     * /////////////////////////////////////////////////////////////////////
      * Returns random value
      * @param index - (optional) index of the random value to get
      * @returns {float}
@@ -58,6 +61,7 @@
     };
 
     /**
+     * /////////////////////////////////////////////////////////////////////
      * Function to generate one new random value over time
      * @type {Function}
      */
@@ -69,11 +73,8 @@
         _randomArray[_currentRandomChangeIndex++] = Math.random();
     };
 
-    if (_options.randomInterval) {
-        setInterval(random.randomChanger, _options.randomInterval);
-    }
-
     /**
+     * /////////////////////////////////////////////////////////////////////
      * Generate random values
      */
     for (var i = 0; i < _options.valuesCount; i++) {
@@ -81,84 +82,86 @@
     }
 
     /**
+     * /////////////////////////////////////////////////////////////////////
+     * Set the random generation loop
+     */
+    if (_options.randomInterval) {
+        setInterval(random.randomChanger, _options.randomInterval);
+    }
+
+    /**
+     * /////////////////////////////////////////////////////////////////////
      * Extends random functions with some values
      */
-    function setRandomExtensions() {
+
+    /**
+     * Random valueOf
+     * @type {Function}
+     */
+    random.valueOf = random;
+
+    /**
+     * Random function options
+     * @type {Object}
+     */
+    random.options = _options;
+
+    /**
+     * If Object.defineProperty is defined then add some getters fo extended api usage
+     */
+    if (typeof Object.defineProperty !== "undefined") {
         /**
-         * Random valueOf
-         * @type {Function}
+         * random.random getter
          */
-        random.valueOf = random;
+        Object.defineProperty(random, 'random', {
+            get: function () {
+                return random();
+            }
+        });
 
         /**
-         * Random function options
-         * @type {Object}
+         * random.randomArray returns current random values array
          */
-        random.options = _options;
+        Object.defineProperty(random, 'randomArray', {
+            get: function () {
+                return _randomArray;
+            }
+        });
 
         /**
-         * If Object.defineProperty is defined then add some getters fo extended api usage
+         * random.currentRandomIndex returns current index where random function is at in array
          */
-        if (typeof Object.defineProperty !== "undefined") {
-            /**
-             * random.random getter
-             */
-            Object.defineProperty(random, 'random', {
-                get: function () {
-                    return random();
-                }
-            });
-
-            /**
-             * random.randomArray returns current random values array
-             */
-            Object.defineProperty(random, 'randomArray', {
-                get: function () {
-                    return _randomArray;
-                }
-            });
-
-            /**
-             * random.currentRandomIndex returns current index where random function is at in array
-             */
-            Object.defineProperty(random, 'currentRandomIndex', {
-                get: function () {
-                    return _currentRandomIndex;
-                }
-            });
-        }
+        Object.defineProperty(random, 'currentRandomIndex', {
+            get: function () {
+                return _currentRandomIndex;
+            }
+        });
     }
 
     /**
+     * /////////////////////////////////////////////////////////////////////
      * Exports random to different roots
      */
-    function setRandomExport() {
-        /**
-         * Export as module
-         */
-        if (typeof module === "object" && typeof module.exports === "object") {
-            module.exports = random;
-        }
-        /**
-         * Register as AMD module
-         */
-        else if (typeof define === "function" && define.amd) {
-            define("fastrandom", [], function () {
-                return random;
-            });
-        }
 
-        /**
-         * If there is a window object, that at least has a document property, define random
-         */
-        if (typeof window === "object" && typeof window.document === "object") {
-            window.random = random;
-        }
+    /**
+     * Export as module
+     */
+    if (typeof module === "object" && typeof module.exports === "object") {
+        module.exports = random;
+    }
+    /**
+     * Register as AMD module
+     */
+    else if (typeof define === "function" && define.amd) {
+        define("fastrandom", [], function () {
+            return random;
+        });
     }
 
     /**
-     * Do the magic ;)
+     * If there is a window object, that at least has a document property, define random
      */
-    setRandomExtensions();
-    setRandomExport();
+    if (typeof window === "object" && typeof window.document === "object") {
+        window.random = random;
+    }
 })();
