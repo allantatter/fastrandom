7792116d18ca43ef1411bdc404372f0cee78e485
Hendry Sadrak <hendry.sadrak@outlook.com>
2015-07-30 02:10:43 +0300

    EDIT Make cli faster by not generating a large array of randoms

--- bin/fastrandom.js
+++ bin/fastrandom.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+global.fastrandomOptions = {
+    valuesCount: 1,
+    randomInterval: false,
+    removeOptions: true
+};
+
 var random = require('./../src/random.js');
 
 console.log(random());
